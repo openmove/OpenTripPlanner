3568a8b4516e98badc18575f3db98eac75218827
Simon Jacobs <sjacobs@camsys.com>
2018-03-12 15:27:01 -0400

    ADDED RT trip patterns should flow through API

--- src/main/java/org/opentripplanner/routing/edgetype/TimetableSnapshot.java
+++ src/main/java/org/opentripplanner/routing/edgetype/TimetableSnapshot.java
@@ -14,10 +14,11 @@
 package org.opentripplanner.routing.edgetype;
 
 import java.util.*;
-import java.util.Map.Entry;
 
+import org.onebusaway.gtfs.model.Stop;
 import org.onebusaway.gtfs.model.calendar.ServiceDate;
 import org.opentripplanner.routing.trippattern.TripTimes;
+import org.opentripplanner.updater.stoptime.AddedTripPatternIndex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -44,55 +45,6 @@ public class TimetableSnapshot {
             return t1.serviceDate.compareTo(t2.serviceDate);
         }
     }
-    
-    /**
-     * Class to use as key in HashMap containing feed id, trip id and service date
-     */
-    protected class TripIdAndServiceDate {
-        private final String feedId;
-        private final String tripId;
-        private final ServiceDate serviceDate;
-        
-        public TripIdAndServiceDate(final String feedId, final String tripId, final ServiceDate serviceDate) {
-            this.feedId = feedId;
-            this.tripId = tripId;
-            this.serviceDate = serviceDate;
-        }
-
-        public String getFeedId() {
-            return feedId;
-        }
-
-        public String getTripId() {
-            return tripId;
-        }
-
-        public ServiceDate getServiceDate() {
-            return serviceDate;
-        }
-
-
-        @Override
-        public int hashCode() {
-            int result = Objects.hash(tripId, serviceDate, feedId);
-            return result;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj)
-                return true;
-            if (obj == null)
-                return false;
-            if (getClass() != obj.getClass())
-                return false;
-            TripIdAndServiceDate other = (TripIdAndServiceDate) obj;
-            boolean result = Objects.equals(this.tripId, other.tripId) &&
-                    Objects.equals(this.serviceDate, other.serviceDate) &&
-                    Objects.equals(this.feedId, other.feedId);
-            return result;
-        }
-    }
 
     private static final Logger LOG = LoggerFactory.getLogger(TimetableSnapshot.class);
     
@@ -103,18 +55,8 @@ public class TimetableSnapshot {
     private HashMap<TripPattern, SortedSet<Timetable>> timetables =
             new HashMap<TripPattern, SortedSet<Timetable>>();
 
-    /**
-     * <p>
-     * Map containing the last <b>added</b> trip pattern given a trip id (without agency) and a
-     * service date as a result of a call to {@link #update(String feedId, TripPattern, TripTimes, ServiceDate)}
-     * with trip times of a trip that didn't exist yet in the trip pattern.
-     * </p>
-     * <p>
-     * This is a HashMap and not a Map so the clone function is available.
-     * </p>
-     */
-    private HashMap<TripIdAndServiceDate, TripPattern> lastAddedTripPattern = new HashMap<>();
-    
+    private AddedTripPatternIndex addedTripPatternIndex = new AddedTripPatternIndex();
+
     /**
      * Boolean value indicating that timetable snapshot is read only if true. Once it is true, it shouldn't
      * be possible to change it to false anymore.
@@ -163,9 +105,7 @@ public class TimetableSnapshot {
      * @return last added trip pattern; null if trip never was added to a trip pattern
      */
     public TripPattern getLastAddedTripPattern(String feedId, String tripId, ServiceDate serviceDate) {
-        TripIdAndServiceDate tripIdAndServiceDate = new TripIdAndServiceDate(feedId, tripId, serviceDate);
-        TripPattern pattern = lastAddedTripPattern.get(tripIdAndServiceDate);
-        return pattern;
+        return addedTripPatternIndex.getLastAddedTripPattern(feedId, tripId, serviceDate);
     }
 
     /**
@@ -217,8 +157,7 @@ public class TimetableSnapshot {
             tt.addTripTimes(updatedTripTimes);
             // Remember this pattern for the added trip id and service date
             String tripId = updatedTripTimes.trip.getId().getId();
-            TripIdAndServiceDate tripIdAndServiceDate = new TripIdAndServiceDate(feedId, tripId, serviceDate);
-            lastAddedTripPattern.put(tripIdAndServiceDate, pattern);
+            addedTripPatternIndex.addTripPattern(feedId, tripId, serviceDate, pattern);
         } else {
             // Set updated trip times of trip
             tt.setTripTimes(tripIndex, updatedTripTimes);
@@ -255,8 +194,7 @@ public class TimetableSnapshot {
             tt.finish(); // summarize, index, etc. the new timetables
         }
         ret.timetables = (HashMap<TripPattern, SortedSet<Timetable>>) this.timetables.clone();
-        ret.lastAddedTripPattern = (HashMap<TripIdAndServiceDate, TripPattern>)
-                this.lastAddedTripPattern.clone();
+        ret.addedTripPatternIndex = this.addedTripPatternIndex.copy();
         this.dirtyTimetables.clear();
         this.dirty = false;
 
@@ -300,7 +238,7 @@ public class TimetableSnapshot {
      * @return true if the lastAddedTripPattern changed as a result of the call
      */
     protected boolean clearLastAddedTripPattern(String feedId) {
-        return lastAddedTripPattern.keySet().removeIf(lastAddedTripPattern -> feedId.equals(lastAddedTripPattern.getFeedId()));
+       return addedTripPatternIndex.clearTripPattern(feedId);
     }
 
     /**
@@ -333,17 +271,18 @@ public class TimetableSnapshot {
         }
         
         // Also remove last added trip pattern for days that are purged
-        for (Iterator<Entry<TripIdAndServiceDate, TripPattern>> iterator = lastAddedTripPattern
-                .entrySet().iterator(); iterator.hasNext();) {
-            TripIdAndServiceDate tripIdAndServiceDate = iterator.next().getKey();
-            if (serviceDate.compareTo(tripIdAndServiceDate.getServiceDate()) >= 0) {
-                iterator.remove();
-                modified = true;
-            }
-        }
+        modified |= addedTripPatternIndex.purgeExpiredData(serviceDate);
 
         return modified;
     }
+
+    /**
+     * Get all added TripPatterns for stop.
+     */
+    public Collection<TripPattern> getTripPatternsForStop(Stop stop) {
+        return addedTripPatternIndex.getTripPatternsForStop(stop);
+    }
+
 
     public boolean isDirty() {
         if (readOnly) return false;

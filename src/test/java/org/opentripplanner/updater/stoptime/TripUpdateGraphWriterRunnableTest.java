5903d07c0af36da5d7c1f042d2880b769f8fa1a5
Simon Jacobs <sjacobs@camsys.com>
2018-03-21 11:15:43 -0400

    fix failing test

--- src/test/java/org/opentripplanner/updater/stoptime/TripUpdateGraphWriterRunnableTest.java
+++ src/test/java/org/opentripplanner/updater/stoptime/TripUpdateGraphWriterRunnableTest.java
@@ -40,6 +40,6 @@ public class TripUpdateGraphWriterRunnableTest {
 
         tripUpdateGraphWriterRunnable.run(graph);
 
-        verify(timetableSnapshotSource).applyTripUpdates(graph, fullDataset, updates, agencyId);
+        verify(timetableSnapshotSource).applyTripUpdates(graph, fullDataset, updates, agencyId, -1);
     }
 }

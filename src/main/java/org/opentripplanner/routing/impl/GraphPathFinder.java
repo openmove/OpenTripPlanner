94f7316013465c2b61c4d29427a4e7c4a55f260b
Derek Edwards <dedwards8@gmail.com>
2019-12-05 15:42:09 -0500

    Ignore required_stop_id rules.

--- src/main/java/org/opentripplanner/routing/impl/GraphPathFinder.java
+++ src/main/java/org/opentripplanner/routing/impl/GraphPathFinder.java
@@ -242,6 +242,7 @@ public class GraphPathFinder {
                 }
 
                 // If this path violates required_stop_id rules ban it
+                /*
                 List<String> previousStops = new ArrayList<String>();
                 Boolean valid = true;
                 for (Edge edge : path.edges) {
@@ -264,6 +265,7 @@ public class GraphPathFinder {
                 if(!valid) {
                     continue;
                 }
+                */
 
                 // Keep Track of the shortest duration of all paths found so far.
                 if (bestDuration == -1.0) {

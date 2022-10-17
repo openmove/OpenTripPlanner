a1cb0de6ec2f82bfdbb86d055c505110847bb7e7
wouldgo <wouldgo84@gmail.com>
2022-10-17 15:24:13 +0200

    graphql zones lists were not cloned

--- src/main/java/org/opentripplanner/model/Zone.java
+++ src/main/java/org/opentripplanner/model/Zone.java
@@ -4,94 +4,98 @@ package org.opentripplanner.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.common.collect.Lists;
+
 public final class Zone extends IdentityBean<FeedScopedId> {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private static final int MISSING_VALUE = -999;
+	private static final int MISSING_VALUE = -999;
 
-    private FeedScopedId id;
+	private FeedScopedId id;
 
-    private String name;
+	private String name;
 
-    private double lat;
+	private double lat;
 
-    private double lon;
+	private double lon;
 
-    private String zoneId;
-    
-    private int isContainsZone = 1;
-    
-    private List<Stop> stops = new ArrayList<>();
-    
-    private List<String> fareIdentifiers = new ArrayList<>();
+	private String zoneId;
 
-    public Zone() {
+	private int isContainsZone = 1;
 
-    }
+	private List<Stop> stops = new ArrayList<>();
 
-    public Zone(Zone obj) {
-        this.id = obj.id;
-        this.name = obj.name;
-        this.lat = obj.lat;
-        this.lon = obj.lon;
-        this.zoneId = obj.zoneId;
-        this.isContainsZone = obj.isContainsZone;
-        this.stops = obj.stops;
-        this.fareIdentifiers = obj.fareIdentifiers;
-    }
+	private List<String> fareIdentifiers = new ArrayList<>();
 
-    public FeedScopedId getId() {
-        return id;
-    }
+	public Zone() {
 
-    public void setId(FeedScopedId id) {
-        this.id = id;
-    }
+	}
 
-    public String getName() {
-        return name;
-    }
+	public Zone(Zone obj) {
+		this.id = obj.id;
+		this.name = obj.name;
+		this.lat = obj.lat;
+		this.lon = obj.lon;
+		this.zoneId = obj.zoneId;
+		this.isContainsZone = obj.isContainsZone;
+		this.stops = Lists.newArrayList(obj.stops.toArray(new Stop[obj.stops.size()]));
+		this.fareIdentifiers = Lists.newArrayList(obj.fareIdentifiers.toArray(new String[obj.fareIdentifiers.size()]));
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	@Override
+	public FeedScopedId getId() {
+		return this.id;
+	}
 
-    public double getLat() {
-        return lat;
-    }
+	@Override
+	public void setId(FeedScopedId id) {
+		this.id = id;
+	}
 
-    public void setLat(double lat) {
-        this.lat = lat;
-    }
+	public String getName() {
+		return this.name;
+	}
 
-    public double getLon() {
-        return lon;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setLon(double lon) {
-        this.lon = lon;
-    }
+	public double getLat() {
+		return this.lat;
+	}
 
-    public String getZoneId() {
-        return zoneId;
-    }
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
 
-    public void setZoneId(String zoneId) {
-        this.zoneId = zoneId;
-    }
+	public double getLon() {
+		return this.lon;
+	}
 
-    public List<Stop> getStops() {
-        return this.stops;
-    }
+	public void setLon(double lon) {
+		this.lon = lon;
+	}
 
-    @Override
-    public String toString() {
-        return "<Zone " + this.id + ">";
-    }
+	public String getZoneId() {
+		return this.zoneId;
+	}
+
+	public void setZoneId(String zoneId) {
+		this.zoneId = zoneId;
+	}
+
+	public List<Stop> getStops() {
+		return this.stops;
+	}
+
+	@Override
+	public String toString() {
+		return "<Zone " + this.id + ">";
+	}
 
 	public List<String> getFareIdentifiers() {
-		return fareIdentifiers;
+		return this.fareIdentifiers;
 	}
 
 	public void setFareIdentifiers(List<String> fareIdentifiers) {
@@ -100,9 +104,9 @@ public final class Zone extends IdentityBean<FeedScopedId> {
 
 	public void setIsContainsZone(int b) {
 		this.isContainsZone = b;
-		
+
 	}
-	
+
 	public int getIsContainsZone() {
 		return this.isContainsZone;
 	}
